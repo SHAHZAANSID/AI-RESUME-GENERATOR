0ba97a38e9ed05f73800055f7a0693c427881bcc
SHAHZAANSID <shahzaansid@gmail.com>
2026-07-28 13:33:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -242,5 +242,6 @@ if st.button('generate resume'):
     response = agent.invoke({'messages': [{'role':'user','content': job_prompt}]})
 
     job_code = response['messages'][-1].content[-1]['text']
+    st.html(job_code, width"stretch", unsafe_allow_javascript=True)
 
 
